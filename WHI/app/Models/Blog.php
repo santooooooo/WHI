ddb101a47722752f0d8eba1e24a076d36e3e4752
santooooooo <santo.shunsuke@gmail.com>
2022-03-17 03:16:40 +0000

    #13 add bolg database

---
+++ WHI/app/Models/Blog.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Blog extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        'section_id',
+        'title',
+        'text',
+    ];
+
+    /**
+     * ユーザーとプロフィールのブログとのデータの関係
+     */
+    public function user(): object
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * プロフィールのセクションとブログとのデータの関係
+     */
+    public function user(): object
+    {
+        return $this->belongsTo(Section::class);
+    }
+}
